6aa870d5bc3ff77309585cfac496b0448b0a0701
f4ilfish <mishka4ernij@mail.ru>
2024-02-05 22:09:05 +0300

    <add> Create / Delete tests

---
+++ Notes.Tests/Common/TestCommandBase.cs
@@ -0,0 +1,19 @@
+﻿using Notes.Persistance;
+
+namespace Notes.Tests.Common
+{
+    public class TestCommandBase : IDisposable
+    {
+        protected readonly NotesDbContext Context;
+
+        public TestCommandBase() 
+        { 
+            Context = NotesContextFactory.Create();
+        }
+
+        public void Dispose() 
+        {
+            NotesContextFactory.Destroy(Context);
+        }
+    }
+}
